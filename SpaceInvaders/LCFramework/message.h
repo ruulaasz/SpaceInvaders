5bc87cd361fe6fbfae6296d2b8c7aef9e0aee16e
Walkever <wowwalkever@gmail.com>
2018-02-17 18:15:06 -0800

    *message

--- SpaceInvaders/LCFramework/message.h
+++ SpaceInvaders/LCFramework/message.h
@@ -8,7 +8,7 @@ static bool PrinteMessage = true;
 static MESSAGE_LOG ReturnCallMessage(MESSAGE_LOG Descriptor, MESSAGE_LOG aditionalText, int line, char* foo, char* file)
 {
 	if(PrinteMessage)
-		std::cout << line << "::" << foo << "::" << file << "::" << Descriptor << "::" << aditionalText << ".\n";
+		std::cout << "\n" <<line << "::" << foo << "::" << file << "::" << Descriptor << "::" << aditionalText << ".\n";
 	return Descriptor;
 }
 
@@ -31,7 +31,7 @@ static MESSAGE_LOG ReturnCallMessage(MESSAGE_LOG Descriptor, MESSAGE_LOG adition
 #define MESSAGE_SUCCESS(...) M_SUCCESS
 #define MESSAGE_WARNING(...) M_WARNING
 
-MESSAGE_LOG ReturnCallMessage(MESSAGE_LOG Descriptor, MESSAGE_LOG aditionalText)
+MESSAGE_LOG ReturnCallMessage(MESSAGE_LOG Descriptor, MESSAGE_LOG aditionalText, int line, char* foo, char* file)
 {
 	return Descriptor;
 }
