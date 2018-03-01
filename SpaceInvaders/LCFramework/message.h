310a6c2de522552d89fdd8fca4502cfff2d83ae8
Walkever <wowwalkever@gmail.com>
2018-02-28 22:55:33 -0800

    * Changes for relese crash

--- SpaceInvaders/LCFramework/message.h
+++ SpaceInvaders/LCFramework/message.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <iostream>
 
-#ifdef _DEBUG
 #define MESSAGE_LOG char*
+#define M_ERROR "ERROR"
+#define M_SUCCESS "SUCCESS"
+#define M_WARNING "WARNING"
+
+#ifdef _DEBUG
+
 
 static bool PrinteMessage = false;
 static inline MESSAGE_LOG ReturnCallMessage(MESSAGE_LOG Descriptor, MESSAGE_LOG aditionalText, int line, char* foo, char* file)
@@ -14,19 +19,12 @@ static inline MESSAGE_LOG ReturnCallMessage(MESSAGE_LOG Descriptor, MESSAGE_LOG 
 
 #define ENABLE_PRINT_MESSAGE(value) PrinteMessage = value 
 #define PPRINT_MESSAGE(Descriptor, aditionalText, line, foo, file) ReturnCallMessage(Descriptor, aditionalText, line, foo, file)
-#define M_ERROR "ERROR"
-#define M_SUCCESS "SUCCESS"
-#define M_WARNING "WARNING"
 #define MESSAGE_ERROR(Text) PPRINT_MESSAGE(M_ERROR , Text, __LINE__, __FUNCTION__, __FILE__)
 #define MESSAGE_SUCCESS(Text) PPRINT_MESSAGE(M_SUCCESS, Text, __LINE__, __FUNCTION__, __FILE__)
 #define MESSAGE_WARNING(Text) PPRINT_MESSAGE(M_WARNING, Text, __LINE__, __FUNCTION__, __FILE__)
 #else
 #define ENABLE_PRINT_MESSAGE(...)
 #define PPRINT_MESSAGE(...) 
-#define MESSAGE_LOG int
-#define M_ERROR 0
-#define M_SUCCESS 1
-#define M_WARNING 2
 #define MESSAGE_ERROR(...) M_ERROR
 #define MESSAGE_SUCCESS(...) M_SUCCESS
 #define MESSAGE_WARNING(...) M_WARNING
